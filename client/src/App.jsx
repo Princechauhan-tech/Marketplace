25ac24752d8b12c5fb550327d9afa04b8a36a3a0
Prince Chauhan <chauhanprince290@gmail.com>
2026-09-22 07:44:48 +0530

    Update developer credits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1669,7 +1669,7 @@ function Footer({ navigate }) {
         <span>
           Developed by{" "}
           <strong className="developer-names">
-            PRINCE CHAUHAN 
+            PRINCE CHAUHAN & Sushant Kumar Ravi
           </strong>
         </span>
       </div>
